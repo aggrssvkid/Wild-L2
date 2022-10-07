628a5f118d126e751cf1a593008c080d587de49a
PaiMei <yaveruvdeada@mail.ru>
2022-10-07 15:50:49 +0600

    add mod files + go mod tidy

---
+++ develop/dev01/go.mod
@@ -0,0 +1,11 @@
+module github.com/aggrssvkid/Wild-L2/develop/dev01
+
+go 1.19
+
+require github.com/beevik/ntp v0.3.0
+
+require (
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20221004154528-8021a29435af // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+)
--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,2 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	ntp "github.com/beevik/ntp"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		_, err := fmt.Fprint(os.Stderr, "Не удалось подключиться к хосту")
+		if err != nil {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
+	fmt.Println(time)
+}
